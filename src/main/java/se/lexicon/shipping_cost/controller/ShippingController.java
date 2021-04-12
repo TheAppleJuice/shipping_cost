ef8e431ba7bff1344383c43450b2e813282d5657
Sebastian Bocaciu <75611075+TheAppleJuice@users.noreply.github.com>
2021-04-12 11:14:58 +0200

    Added Controller and GetMapping to ShippingController

--- src/main/java/se/lexicon/shipping_cost/controller/ShippingController.java
+++ src/main/java/se/lexicon/shipping_cost/controller/ShippingController.java
@@ -1,4 +1,14 @@
 package se.lexicon.shipping_cost.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class ShippingController {
+
+    @GetMapping("/")
+    public String addBoxForm(){
+        return "addBoxForm";
+    }
+
 }
