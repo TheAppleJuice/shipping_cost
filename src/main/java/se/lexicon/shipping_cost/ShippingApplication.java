036091e975085aaee8a1deaad620b596cc005e30
Sebastian Bocaciu <75611075+TheAppleJuice@users.noreply.github.com>
2021-04-12 09:42:21 +0200

    run application

---
+++ src/main/java/se/lexicon/shipping_cost/ShippingApplication.java
@@ -0,0 +1,11 @@
+package se.lexicon.shipping_cost;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShippingApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ShippingApplication.class, args);
+    }
+}
